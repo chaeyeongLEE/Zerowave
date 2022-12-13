0fd65e30b77ccfd0f5e1f51d56da6d0b78256b21
hatbann <0urloveis_great@naver.com>
2022-12-13 16:08:07 +0900

    feat:조혜빈 main router만들기

---
+++ zerowave/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+app.set('view engine', 'ejs');
+
+app.use('/static', express.static(__dirname + '/static'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const router = require('./routes');
+app.use('/zerowave', router);
+
+app.get('*', (req, res) => {
+    res.send('주소가 존재하지 않습니다. 다시 한 번 확인해주세요.');
+  });
+  
+
+
+app.listen(process.env.PORT, () => {
+    console.log('server open: ', process.env.PORT);
+  });
+  
