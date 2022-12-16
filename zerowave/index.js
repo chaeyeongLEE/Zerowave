c2e18a64f57da01dc79246bd7b65961e78d38725
raelulu <raelulu23@gmail.com>
2022-12-16 19:16:12 +0900

    feat: joinjoin

--- zerowave/index.js
+++ zerowave/index.js
@@ -17,7 +17,7 @@ app.use('/zerowave', router);
 // app.get('*', (req, res) => {
 //     res.send('주소가 존재하지 않습니다. 다시 한 번 확인해주세요.');
 //   });
-  
+
 app.get('/map',(req, res)=>{
     res.render("map");
     
