87c1fbf9aec105ceb229a43cdb7cdbba1a75f22b
raelulu <raelulu23@gmail.com>
2022-12-17 18:54:43 +0900

    feat:12/17

--- zerowave/controller/Cuser.js
+++ zerowave/controller/Cuser.js
@@ -5,81 +5,17 @@ const bcrypt = require("bcryptjs");
 
 
 exports.join = (req, res) => {
-  let sessionInputData = req.session.inputData;
 
-  if (!sessionInputData) {
-    sessionInputData = {
-      hasError: false,
-      email: "",
-      password: "",
-      confirmPassword: "",
-    };
-  }
-  req.session.user = null;
-
-  res.render("join", { inputData: sessionInputData });
+  res.render("join");
 };
 
 
 
 
-exports.postJoin = async (req, res) => {
-  const enteredEmail = req.body.email;
-  const enteredPassword = req.body.pw;
-  const enteredConfirmPassword = req.body.confirmpw;
-  const enteredName = req.body.name;
-
-  if (
-    !enteredEmail ||
-    !enteredPassword ||
-    !enteredConfirmPassword ||
-    !enteredName ||
-    enteredPassword.trim() > 16 ||
-    enteredPassword.trim() < 8 ||
-    enteredConfirmPassword.trim() > 16 ||
-    enteredConfirmPassword.trim() < 8 ||
-    enteredPassword !== enteredConfirmPassword
-  ) {
-    req.session.inputData = {
-      hasError: true,
-      message: "형식이 맞지 않습니다. 다시 한번 확인해주세요!",
-      email: enteredEmail,
-      pw: enteredPassword,
-    };
-
-    req.session.save(function () {
-      res.redirect("/zerowave/join");
-    });
-    return;
+exports.postJoin = (req, res) => {
+ 
+    res.redirect("/zerowave/join");
   }
-
-  const existingUser = await User.findOne({ where: { email: enteredEmail } });
-
-  if (existingUser) {
-    req.session.inputData = {
-      hasError: true,
-      message: "동일한 이메일이 이미 존재합니다!",
-      email: enteredEmail,
-      pw: enteredPassword,
-    };
-    req.session.save(function () {
-      res.redirect("/zerowave/join");
-    });
-    return;
-  }
-
-  const hashedPassword = await bcrypt.hash(req.body.pw, 12);
-
-  let data = {
-    user_email: enteredEmail,
-    user_pw: hashedPassword,
-    user_name: enteredName,
-  };
-
-  const user = await User.create(data);
-  res.redirect("/zerowave");
-};
-
 
 
 //
