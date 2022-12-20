10f3d0acaad07a6cc0be30aa178802c2986af05c
eeuih <neuih2@gmail.com>
2022-12-20 19:32:55 +0900

    map controller

--- zerowave/controller/Cmap.js
+++ zerowave/controller/Cmap.js
@@ -5,22 +5,24 @@ exports.index = (req,res) => { res.render("map"); }
 
 exports.zwShopList = async(req,res) => {
 
-    // var currentLocate = {
-    //     top: req.body.latlonData.swLat,
-    //     left: req.body.latlonData.swLon,
-    //     bottom: req.body.latlonData.neLat,
-    //     right: req.body.latlonData.neLon   
-    // };
+    console.log(req.body);
+
+    var currentLocate = {
+        top: req.body.swLat,
+        left: req.body.swLng,
+        bottom: req.body.neLat,
+        right: req.body.neLng   
+    };
+
+    console.log(currentLocate);
 
     var zwshopMap = await zwMap.findAll({
         raw: true,
-        // where: { 
-        //     lat : { [Op.between]: [currentLocate.top, currentLocate.bottom]},
-        //     lon : { [Op.between]: [currentLocate.left, currentLocate.right]}
-        // }
+         where: { 
+             lat : { [Op.between]: [currentLocate.top, currentLocate.bottom]},
+             lon : { [Op.between]: [currentLocate.left, currentLocate.right]}
+         }
     });
     
-    
-
     res.send(zwshopMap); 
 }
