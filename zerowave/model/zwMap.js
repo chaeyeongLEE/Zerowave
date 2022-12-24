d674a8b8e86f276dda7b198986ee424b0f968426
chaeyeongLEE <dh503401@naver.com>
2022-12-24 16:20:35 +0900

    mapnew

--- zerowave/model/zwMap.js
+++ zerowave/model/zwMap.js
@@ -1,47 +1,39 @@
 const zwMap = (Sequelize, DataTypes)=>{
-
-    return Sequelize.define(
-        "zwMap",
-        {
-          id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-          },
-
-          spot_name: { // name varchar(10) not null
-                type: DataTypes.TEXT,
-                allowNull: false
-            },
-    
-            address: { // comment mediumtext 
-                type: DataTypes.TEXT,
-                allowNull: false
-            },
-
-            lat: {
-                type: DataTypes.DOUBLE,
-                allowNull: false
-            },
-
-            lon: {
-                type: DataTypes.DOUBLE,
-                allowNull: false
-            },
-
-            user_id: {
-              type: DataTypes.STRING(45),
-              allowNull: false
-          }
+  return Sequelize.define(
+      "zwMap",
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
         },
-
-        {
-          tableName: "zwMap",
-          freezeTableName: true,
-          timestamps: false,
+        spot_name: { // name varchar(10) not null
+              type: DataTypes.TEXT,
+              allowNull: false
+          },
+          address: { // comment mediumtext
+              type: DataTypes.TEXT,
+              allowNull: false
+          },
+          lat: {
+              type: DataTypes.DOUBLE,
+              allowNull: false
+          },
+          lon: {
+              type: DataTypes.DOUBLE,
+              allowNull: false
+          },
+          user_email: {
+            type: DataTypes.STRING(255),
+            allowNull: false
         }
-      );
-    };
-
+      },
+      {
+        tableName: "zwMap",
+        freezeTableName: true,
+        timestamps: false,
+      }
+    );
+  };
 module.exports = zwMap;
